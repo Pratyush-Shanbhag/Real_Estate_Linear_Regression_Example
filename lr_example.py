66f9fb13a853797fadaedc3911107fa039788581
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2021-07-18 22:53:03 -0700

    Added seaborn visualization subplots

--- lr_example.py
+++ lr_example.py
@@ -15,4 +15,11 @@ redata.drop(redata.columns[[0, 5, 6]], axis=1, inplace=True)
 
 print(redata.head())
 
+fig, axs = plt.subplots(ncols=4)
+num = 0
+for col in redata.columns:
+	if col != 'house_price':
+		sns.scatterplot(x=col, y='house_price', data=redata, ax=axs[num])
+		num = num + 1
 
+plt.show()
