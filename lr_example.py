03692a12cff262e7ac5f41cf4545b82c79294387
Pratyush Shanbhag <49264659+Pratyush-Shanbhag@users.noreply.github.com>
2021-07-19 22:11:54 -0700

    Add comments to file for clarity

--- lr_example.py
+++ lr_example.py
@@ -5,16 +5,21 @@ import matplotlib.pyplot as plt
 file_path = 'real_estate.csv'
 redata = pd.read_csv(file_path)
 
+# print list of columns
 print(redata.columns)
 
+# ['No', 'transaction_date', 'house_age', 'distance_nearest_MRT_station', 'num_convenience_stores', 'latitude', 'longitude', 'house_price']
+
+# print first 10 rows of data
 print(redata.head())
 
-#['No', 'transaction_date', 'house_age', 'distance_nearest_MRT_station', 'num_convenience_stores', 'latitude', 'longitude', 'house_price']
-
+# drop the useless data columns
 redata.drop(redata.columns[[0, 5, 6]], axis=1, inplace=True)
 
+# print first 10 rows of data after modification
 print(redata.head())
 
+# print subplots
 fig, axs = plt.subplots(ncols=4)
 num = 0
 for col in redata.columns:
